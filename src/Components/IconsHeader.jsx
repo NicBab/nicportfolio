0ce01645d930834c902523d931a6faf06677b506
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-09-29 22:14:02 -0500

    add insta && fb icons

--- src/Components/IconsHeader.jsx
+++ src/Components/IconsHeader.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-import { gitHubIconHead, linkedinIconHead } from '../icons/index'
 import './css/IconsHeader.css'
+import { 
+    gitHubIconHead,
+    linkedinIconHead,
+    facebookIconHead,
+    instaIconHead 
+} from '../icons/index'
+
 
 const IconsHeader = () => {
 
@@ -11,11 +17,21 @@ const IconsHeader = () => {
     const onLinkedinClick = () => {
         window.open("https://www.linkedin.com/in/nic-babineaux/")
     }
+
+    const onFacebookIconClick = () => {
+        window.open("")
+    }
+
+    const onInstaClick = () => {
+        window.open("")
+    }
     
     return (
         <div id="iconsHead">
             <div className="gitHubIconHeader" onClick={onGitHubClick}>{gitHubIconHead}</div>
             <div className="linkedinIconHeader" onClick={onLinkedinClick}>{linkedinIconHead}</div>
+            <div className="facebookIconHeader" onClick={onFacebookIconClick}>{facebookIconHead}</div>
+            <div className="instaIconHeader" onClick={onInstaClick}>{instaIconHead}</div>
         </div>
     )
 }
