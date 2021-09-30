6486c4abd69af9dd806c313c06dd34ae1dff6e69
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-09-29 23:23:51 -0500

    contact links window.location

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -12,13 +12,21 @@ const Contact = () => {
     window.open("https://github.com/NicBab")
   }
 
+  const onContactPhoneClick = () => {
+    window.location = "tel:+3372579202"
+  }
+
+  const onContactEmailClick = () => {
+    window.location.href="mailto: nickpbab@gmail.com"
+  }
+
     return (
       <>
         <div className="contactPg">Contact Me
           <div className="contactLinkedIn" onClick={onContactLinkedInClick}>LinkedIn</div>
-          <div className="contactgitHub" onClick={onContactGitHubClick}>GitHub</div>
-          <div className="contactEmail">{mailIcon}nickpbab@gmail.com</div>
-          <div className="contactPhone">{phoneIcon}(337)257-9202</div>
+          <div className="contactGitHub" onClick={onContactGitHubClick}>GitHub</div>
+          <div className="contactEmail" onClick={onContactEmailClick}>{mailIcon}nickpbab@gmail.com</div>
+          <div className="contactPhone" onClick={onContactPhoneClick}>{phoneIcon}(337)257-9202</div>
         </div>
       </>
     )
