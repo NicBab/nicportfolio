6d452ec2afe97323a1535487f3c0383e4cfc638c
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-09-30 00:34:44 -0500

    icons adjust

--- src/Components/IconsHeader.jsx
+++ src/Components/IconsHeader.jsx
@@ -10,28 +10,28 @@ import {
 
 const IconsHeader = () => {
 
-    const onGitHubClick = () => {
-        window.open("https://github.com/NicBab")
-    }
-
     const onLinkedinClick = () => {
         window.open("https://www.linkedin.com/in/nic-babineaux/")
+    }
+
+    const onGitHubClick = () => {
+        window.open("https://github.com/NicBab")
     }
 
     const onFacebookIconClick = () => {
         window.open("")
     }
 
-    const onInstaClick = () => {
-        window.open("")
-    }
+    // const onInstaClick = () => {
+    //     window.open("")
+    // }
     
     return (
         <div id="iconsHead">
-            <div className="gitHubIconHeader" onClick={onGitHubClick}>{gitHubIconHead}</div>
             <div className="linkedinIconHeader" onClick={onLinkedinClick}>{linkedinIconHead}</div>
+            <div className="gitHubIconHeader" onClick={onGitHubClick}>{gitHubIconHead}</div>
             <div className="facebookIconHeader" onClick={onFacebookIconClick}>{facebookIconHead}</div>
-            <div className="instaIconHeader" onClick={onInstaClick}>{instaIconHead}</div>
+            {/* <div className="instaIconHeader" onClick={onInstaClick}>{instaIconHead}</div> */}
         </div>
     )
 }
