228d4d99a099afefb9ceed7adf9658413c9c6098
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-10-11 10:22:25 -0500

    added self img to about. Also made header font resoponsive size

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import './css/Header.css'
-import IMG_3364 from '../Images/IMG_3364.jpeg'
 
 const Header = () => {
     return (
       <div className="Header">
        <div className="title">
-       <img
-          className="treeImg"
-          style={{maxWidth: "4em", height: "auto"}}
-          src={IMG_3364}
-          alt="digital tree"
-        />
          Nic Babineaux'
+         <div className="subTitle">Portfolio Site</div>
         </div>
-        <div className="subTitle">Portfolio Site</div>
       </div>
     )
 }
